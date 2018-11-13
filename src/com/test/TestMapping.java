c16e0b04e381f5d4b6b55acaf8771c852ec127d2
vishal javalkar <vishaljavalkar@vishals-MacBook-Pro.local>
2018-11-13 14:39:44 -0500

    first change into. git

---
+++ src/com/test/TestMapping.java
@@ -0,0 +1,64 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class TestMapping {
+
+	public static void main(String[] args) {
+		String[] inputu = {};
+		String[] type = {};
+		String[] eventtype = {};
+		String[] buom = {};
+		String[] auom = {};
+		String[] MAflag = {};
+
+	}
+
+	public void N4(String[] inputu, String[] inputa, String[] type, String[] eventtype, String[] buom, String[] auom,
+			String[] MAflag, ArrayList result) {
+		for (int k = 0; k < eventtype.length; k++) {
+			if (eventtype[k].equalsIgnoreCase("UPDATE") && !MAflag[k].equals("Y")) {
+				for (int y = 0; y < inputu.length; y++) {
+					for (int j = 0; j < auom.length; j++) {
+						if (auom[j] == buom[j]) {
+							// for(int l=0;l<inputu.length;l++)
+							// {
+							try {
+								if (type[y].equalsIgnoreCase("N4") || type[y].equalsIgnoreCase("ND")
+										|| type[y].equalsIgnoreCase("GT")) {
+									result.add(inputu[y]);
+								}
+							} catch (Exception e) {
+							}
+							// }
+						} else {
+							if (type[y].equalsIgnoreCase("GT")) {
+								result.add(inputu[y]);
+							} else {
+
+								if (type[y].equalsIgnoreCase("ND") || type[y].equalsIgnoreCase("N4")) {
+									result.add("_SUPPRESS_");
+								} else {
+
+									result.add("/");
+								}
+							}
+
+						}
+					}
+
+				}
+			} else {
+				for (int i = 0; i < inputa.length; i++) {
+					if (!inputa[i].equals("")) {
+						result.add(inputa[i]);
+					} else {
+						result.add("/");
+					}
+				}
+			}
+		}
+
+	}
+
+}
